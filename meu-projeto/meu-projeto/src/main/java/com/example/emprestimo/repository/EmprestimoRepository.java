fb32c2178980993879b1277b2bb976486d977f58
Yudy <106198975+yudyAS@users.noreply.github.com>
2025-06-02 20:20:27 -0300

    Delete meu-projeto/meu-projeto directory

--- meu-projeto/meu-projeto/src/main/java/com/example/emprestimo/repository/EmprestimoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.emprestimo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.emprestimo.entities.Emprestimo;
-
-public interface EmprestimoRepository extends JpaRepository<Emprestimo, Long>{
-
-}
--- meu-projeto/meu-projeto/src/main/resources/import.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO tb_cliente (renda_mensal, score, nome_completo) VALUES (2500.00, 650, 'João_da_Silva');
-
-INSERT INTO tb_cliente (nome_completo, renda_mensal, score)VALUES ('Maria_Souza', 3000.00, 250);
-
-
-INSERT INTO tb_emprestimo (juros_ano_percentual, quantidade_maxima_parcelas, valo_parcela_mensal, valor_cliente_solicitado, cliente_id, status) VALUES (7.00, 12, 259.58, 3000.00, 1, 'APROVADO');
-
-INSERT INTO tb_emprestimo (juros_ano_percentual, quantidade_maxima_parcelas, valo_parcela_mensal, valor_cliente_solicitado, cliente_id, status) VALUES (7.00, 12, 178.40, 2000.00, 2, 'REPROVADO');
